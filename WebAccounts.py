67a01260c0ab11d8a89d86dc1c1edb8cd47e92f0
Tim Hessels <timhessels@hotmail.com>
2019-04-18 12:35:56 +0200

    Update WebAccounts.py

--- WebAccounts.py
+++ WebAccounts.py
@@ -9,12 +9,12 @@ Created on Thu Mar 17 09:27:36 2016
 def Accounts(Type=None):
 
     User_Pass = {
-     'NASA': ['TimHessels', 'WAteam1!'],
-     'GLEAM': ['gleamuser', 'hi_GLEAMv32#HiL?'],
-     'FTP_WA': ['THessels', 'painole_2016!'],
-     'MSWEP': ['THessels', '9rvkvkG6z'],
-     'Copernicus': ['timhessels91', 'EelsBloc91!2'],  #https://land.copernicus.vgt.vito.be/PDF/
-     'VITO': ['TimHessels91WA', 'EelsBloc91!2']}     #https://www.vito-eodata.be/PDF/datapool/
+     'NASA': ['', ''],
+     'GLEAM': ['', ''],
+     'FTP_WA': ['', ''],
+     'MSWEP': ['', ''],
+     'Copernicus': ['', ''],  #https://land.copernicus.vgt.vito.be/PDF/
+     'VITO': ['', '']}     #https://www.vito-eodata.be/PDF/datapool/
 	 
     Selected_Path = User_Pass[Type]
 
